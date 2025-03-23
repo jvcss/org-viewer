899e770032830b648d05c927a07854f6aaf5b2a0
jVCss <jvcs.mecatronica@gmail.com>
2025-03-23 20:38:19 -0300

    feature moving tree

--- frontend/src/MyOrgChart.jsx
+++ frontend/src/MyOrgChart.jsx
@@ -18,17 +18,32 @@ const MyOrgChart = () => {
       .catch((error) => console.error("Erro ao buscar os dados:", error));
   }, []);
 
-  // Inicializa o orgchart quando os dados estiverem disponíveis
+  // Inicializa o orgchart e anexa o evento de drop
   useEffect(() => {
     if (chartData) {
       // Limpa o container antes de inicializar
       $("#chart-container").empty();
-      $("#chart-container").orgchart({
+
+      // Inicializa o orgchart com as configurações desejadas
+      const oc = $("#chart-container").orgchart({
         data: chartData,
-        //verticalLevel: 8, // quantidade de colunas verticais (default: not responsive)
         nodeContent: "title",
+        draggable: true,
+      });
+
+      // Adiciona o listener para o evento de drop
+      oc.$chart.on("nodedrop.orgchart", function (event, extraParams) {
+        console.log(
+          "draggedNode:" +
+            extraParams.draggedNode.children(".title").text() +
+            ", dragZone:" +
+            extraParams.dragZone.children(".title").text() +
+            ", dropZone:" +
+            extraParams.dropZone.children(".title").text()
+        );
       });
     }
+
     // Função de cleanup: limpa o container ao desmontar ou atualizar
     return () => {
       $("#chart-container").empty();
