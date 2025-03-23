c7c4d2c034ffc53456477eafa17dea0d00eb51cc
jVCss <jvcs.mecatronica@gmail.com>
2025-03-23 12:00:48 -0300

    php é ruim. dificil de configurar e não presta.

--- orgchart-backend/routes/web.php
+++ orgchart-backend/routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/teste', function () {
+    return 'Teste OK';
+});
+
+Route::get('/orgchart', function () {
+    return response()->json([
+        "id" => "1",
+        "name" => "CEO",
+        "title" => "general manager",
+        "children" => [
+            ["id" => "2", "name" => "CTO", "title" => "department manager"],
+        ],
+    ]);
+});
